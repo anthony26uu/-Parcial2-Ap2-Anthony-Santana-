881a4af4d524c6f6ca9cb51fd3855b5af32a52ac
Anthony Javier Santana Polanco <anthony_santana@ucne.edu.do>
2017-11-22 22:38:10 -0400

    ....

--- [Parcial2-Ap2-Anthony Santana]/Ui/Consultas/CPresupuesto.aspx
+++ [Parcial2-Ap2-Anthony Santana]/Ui/Consultas/CPresupuesto.aspx
@@ -3,8 +3,88 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
-    <h1>
-        Prueba  dentro 
-    </h1>
+    
+       <header>
+            <h1 class="page-header text-center">
+
+                  Consulta Clientes <span ></span></h1>
+        </header>
+
+          <script src="../../Scripts/popper.min.js"></script>
+     <!-- Librerias para Toastr -->
+    <link href="/../Content/toastr.css" rel="stylesheet" />
+    <script src="../../Scripts/toastr.js"></script>
+    <script src="../../Scripts/jquery-3.2.1.js"></script>
+
+ <div class= "text-center">
+     
+          <span class="btn btn-danger">Selecione-</span>
+          <asp:DropDownList ID="DropFiltro" CssClass="btn-success" runat="server" Height="45px" OnSelectedIndexChanged="DropFiltro_SelectedIndexChanged"> 
+            
+              <asp:ListItem>Todo</asp:ListItem>
+              <asp:ListItem>ID</asp:ListItem>
+              <asp:ListItem>Fecha</asp:ListItem>
+              <asp:ListItem>Nombre</asp:ListItem>
+              <asp:ListItem>Categoria</asp:ListItem>
+          </asp:DropDownList>
+      
+               
+
+       
+        
+           
+          <br />
+      
+               
+
+       
+        
+           
+          <span class="btn btn-danger">ID/Nombre</span>
+          <asp:TextBox ID="buscaText" runat="server"    class="input-lg" placeholder="ID a buscar" Height="45px" ></asp:TextBox>
+         <asp:Button ID="Button1" runat="server" Text="Buscar" Height="45px" Width="88px" CssClass="btn btn-info" OnClick="Button1_Click"  />
+ 
+            
+  
+          <br />
+
+  
+         <span class="btn btn-danger">Desde</span>
+          
+          
+          <asp:TextBox ID="desdeFecha"  class="input-lg" TextMode="Date"  runat="server" Width="212px" Height="45px"></asp:TextBox>
+          <span class="btn btn-danger">Hasta</span>
+            
+            <asp:TextBox ID="hastaFecha"   class="input-lg" TextMode="Date"  runat="server" Height="44px" Width="212px"></asp:TextBox>
+ 
+              <br />
+            
+            
+  
+
+  
+          <br />    
+     
+             
+       
+     </div>
+
+          <div class="center-block" style="overflow: auto; width: 1434px; height: 315px;">
+
+
+
+                <asp:GridView ID="presupuestoGrid" runat="server"   CssClass=" table table-striped table-hover table-bordered"  HorizontalAlign="Center" Height="244px" Width="942px" >   
+                </asp:GridView>
+        
+    </div>
+    
+      <br />
+        <a href="../Reportes/Ventanas/Clientereport.aspx" class="btn btn-info">
+          <span ></span> Imprimir
+        </a> 
+       
+               <br />
+    <br />
+     
 
 </asp:Content>
--- [Parcial2-Ap2-Anthony Santana]/Ui/Consultas/CPresupuesto.aspx.cs
+++ [Parcial2-Ap2-Anthony Santana]/Ui/Consultas/CPresupuesto.aspx.cs
@@ -9,9 +9,104 @@ namespace _Parcial2_Ap2_Anthony_Santana_.Ui.Consultas
 {
     public partial class CPresupuesto : System.Web.UI.Page
     {
+        public static List<Entidades.PresupuestoDetalles> Lista { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            Lista = BLL.PresupuestoDetalleBLL.GetListodo();
+            Entidades.PresupuestoDetalles detalle = new Entidades.PresupuestoDetalles();
+            presupuestoGrid.DataSource = Lista;
+            presupuestoGrid.DataBind();
+
+            buscaText.Focus();
+
 
         }
+
+        public void Selecionar(int id)
+        {
+
+            if (DropFiltro.SelectedIndex == 0)
+            {
+                buscaText.Text = "";
+                if (Lista.Count == 0)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('No se ha registrado nada ');</script>");
+
+                    buscaText.Text = "";
+                    buscaText.Focus();
+                }
+                else
+                {
+                    Lista = BLL.PresupuestoDetalleBLL.GetListodo();
+                    presupuestoGrid.DataSource = Lista;
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('Resultados ');</script>");
+
+
+                }
+            }
+            else if (DropFiltro.SelectedIndex == 1)
+            {
+
+                Lista = BLL.PresupuestoDetalleBLL.GetList(p => p.PresupuestoId == id);
+
+                if (Lista.Count == 0)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('No se ha registrado ');</script>");
+                    buscaText.Text = "";
+                    buscaText.Focus();
+                }
+                else
+                {
+
+                    presupuestoGrid.DataSource = Lista;
+                    presupuestoGrid.DataBind();
+
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('Rultados ');</script>");
+
+                }
+
+
+
+            }
+       
+            else if (DropFiltro.SelectedIndex == 3)
+            {
+                if (buscaText.Text == "")
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('No existe ');</script>");
+
+
+                    buscaText.Text = "";
+                    buscaText.Focus();
+                }
+                else
+                {
+                    Lista = BLL.PresupuestoDetalleBLL.GetList(p => p.Descripcion == buscaText.Text);
+                    if (Lista.Count == 0)
+                    {
+                        Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('Error ');</script>");
+
+                        buscaText.Text = "";
+                        buscaText.Focus();
+                    }
+                    else
+                    {
+                        presupuestoGrid.DataSource = Lista;
+                        presupuestoGrid.DataBind();
+                        Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script>alert('Rultados ');</script>");
+
+                    }
+
+
+                }
+
+            }
+
+
+
+        }
+
+
     }
 }
